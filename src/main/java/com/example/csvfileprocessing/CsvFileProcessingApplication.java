a749899976aa36303d12b9ca40f25defa272f1e4
KarthikSJ97 <karthik.s.jhingade@gmail.com>
2021-05-14 16:43:21 +0530

    [Initial setup] | feature | Added folder structure

---
+++ src/main/java/com/example/csvfileprocessing/CsvFileProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.example.csvfileprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvFileProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvFileProcessingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/csvfileprocessing/CsvFileProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.csvfileprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvFileProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
